fb3e8c9584781683d7bb114e39f5c35ddbde73e8
tazkir <tazkirfurqan77@gmail.com>
2024-12-08 06:02:20 +0800

    whyus card comp, magicui particles

---
+++ src/components/Card/BenefitsCard.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Image from "next/image";
+import Bg from "../../../public/images/bg-grid.png";
+import dynamic from "next/dynamic";
+import Particles from "../MagicUi/particles";
+
+const Lottie = dynamic(() => import("react-lottie"), { ssr: false });
+
+type LottieOptions = {
+  loop: boolean;
+  autoplay: boolean;
+  animationData: object;
+};
+
+type CardProps = {
+  title: string;
+  desc: string;
+  lottieFile: LottieOptions;
+};
+
+export default function BenefitsCard({ title, desc, lottieFile }: CardProps) {
+  return (
+    <div className="w-[20em] h-[25em] bg-radial-button from-[#262626] to-[#141414] flex flex-col gap-5 justify-start items-center rounded-2xl px-6 py-6 text-center bg-primary shadow-lg">
+      <div className="relative overflow-hidden w-full h-[48%] flex justify-center items-center bg-radial-button from-[#171717] to-[#050505] rounded-xl">
+        <Particles
+          className="absolute inset-0"
+          quantity={50}
+          ease={80}
+          color={"#ffffff"}
+          staticity={100}
+          refresh
+        />
+
+        <div className="absolute w-full h-full top-0">
+          <Image
+            src={Bg}
+            width={100}
+            height={100}
+            alt=""
+            className="object-contain opacity-30 w-full h-full"
+          />
+        </div>
+
+        <div className="w-20 h-20 z-30 rounded-full flex justify-center items-center bg-radial-button from-[#E89548] to-[#E46F03] border border-[#E89548]">
+          <Lottie options={lottieFile} height={100} width={100} />
+        </div>
+      </div>
+      <h3 className="text-2xl font-satoshi">{title}</h3>
+
+      <p className="font-manrope font-normal text-lg text-[#AFAFAF]">{desc}</p>
+    </div>
+  );
+}
--- src/components/Section/WhyUs/WhyUs.tsx
+++ src/components/Section/WhyUs/WhyUs.tsx
@@ -1,18 +1,14 @@
 "use client";
 
 import { Badge } from "@/components/ui/badge";
-import Bg from "../../../../public/images/bg-grid.png";
-import Image from "next/image";
 import brain from "../../../../public/lotties/brain.json";
 import people from "../../../../public/lotties/people.json";
 import thumbup from "../../../../public/lotties/thumbup.json";
-import dynamic from "next/dynamic";
 import { DescriptionText, TitleText } from "@/components/Typography/text";
 import AccentButtonDown from "@/components/Button/AccentButtonDown";
 import { scrollToSection } from "@/lib/scrollToSection";
 import Link from "next/link";
-
-const Lottie = dynamic(() => import("react-lottie"), { ssr: false });
+import BenefitsCard from "@/components/Card/BenefitsCard";
 
 export default function WhyUs() {
   const brainLottie = {
@@ -54,73 +50,27 @@ export default function WhyUs() {
         <DescriptionText title="That drives impactful gain powerful results" />
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 justify-center items-center gap-6 *:w-[20em] *:h-[25em] *:bg-gray-500 *:flex *:flex-col *:gap-5 *:justify-start *:items-center *:rounded-2xl *:px-6 *:py-6 *:text-center *:bg-primary *:shadow-lg">
-        <div>
-          <div className="relative overflow-hidden w-full h-[48%] flex justify-center items-center bg-radial-button from-[#171717] to-[#050505] rounded-xl">
-            <div className="absolute w-full h-full top-0">
-              <Image
-                src={Bg}
-                width={100}
-                height={100}
-                alt=""
-                className="object-contain opacity-30 w-full h-full"
-              />
-            </div>
-            <div className="w-20 h-20 z-20 rounded-full flex justify-center items-center bg-radial-button from-[#E89548] to-[#E46F03] border border-[#E89548]">
-              <Lottie options={brainLottie} height={100} width={100} />
-            </div>
-          </div>
-          <h3 className="text-2xl font-satoshi">Innovative Approach</h3>
+      <div className="grid grid-cols-1 lg:grid-cols-3 justify-center items-center gap-6 ">
+        <BenefitsCard
+          title="Innovative Approach"
+          desc="Look for works that reflect a unique character and differentiate in
+            a crowded marketplace"
+          lottieFile={brainLottie}
+        />
 
-          <p className="font-manrope font-normal text-lg text-[#AFAFAF]">
-            Look for works that reflect a unique character and differentiate in
-            a crowded marketplace
-          </p>
-        </div>
+        <BenefitsCard
+          title="Seamless Experience"
+          desc="A seamless user experience across all devices, ensuring every
+            interaction connects with the user."
+          lottieFile={thumbUpLottie}
+        />
 
-        <div>
-          <div className="relative overflow-hidden w-full h-[48%] flex justify-center items-center bg-radial-button from-[#171717] to-[#050505] rounded-xl">
-            <div className="absolute w-full h-full top-0">
-              <Image
-                src={Bg}
-                width={100}
-                height={100}
-                alt=""
-                className="object-contain opacity-30 w-full h-full"
-              />
-            </div>
-            <div className="w-20 h-20 rounded-full flex justify-center items-center bg-radial-button from-[#E89548] to-[#E46F03] border border-[#E89548]">
-              <Lottie options={thumbUpLottie} height={100} width={100} />
-            </div>
-          </div>
-          <h3 className="text-2xl font-medium">Seamless Experience</h3>
-          <p className="font-manrope font-normal text-lg text-[#AFAFAF]">
-            A seamless user experience across all devices, ensuring every
-            interaction connects with the user.
-          </p>
-        </div>
-
-        <div>
-          <div className="relative overflow-hidden w-full h-[48%] flex justify-center items-center bg-radial-button from-[#171717] to-[#050505] rounded-xl">
-            <div className="absolute w-full h-full top-0">
-              <Image
-                src={Bg}
-                width={100}
-                height={100}
-                alt=""
-                className="object-contain opacity-20 w-full h-full"
-              />
-            </div>
-            <div className="w-20 h-20 rounded-full flex justify-center items-center bg-radial-button from-[#E89548] to-[#E46F03] border border-[#E89548]">
-              <Lottie options={peopleLottie} height={100} width={100} />
-            </div>
-          </div>
-          <h3 className="text-2xl font-medium">Ongoing Partnership</h3>
-          <p className="font-manrope font-normal text-lg text-[#AFAFAF]">
-            Find a new partner easily, not just providers, who offer ongoing
-            support even after the project ends.
-          </p>
-        </div>
+        <BenefitsCard
+          title="Ongoing Partnership"
+          desc="Find a new partner easily, not just providers, who offer ongoing
+            support even after the project ends."
+          lottieFile={peopleLottie}
+        />
       </div>
 
       <Link
